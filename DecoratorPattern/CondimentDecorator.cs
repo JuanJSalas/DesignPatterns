ea1e5136012bf4d3b1efad23ccc93561eb40361d
juanj <privatestringjjs@gmail.com>
2024-03-07 12:24:27 -0600

    Added new project and its base classes

---
+++ DecoratorPattern/CondimentDecorator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPattern
+{
+    public abstract class CondimentDecorator : Beverage
+    {
+        public Beverage _beverage;
+
+        public abstract string GetDescription();
+    }
+}
